1aa9ac4f6e555a0dd74fd0ed76ccc733913ace7e
kostet499 <kostet499@gmail.com>
2018-11-09 22:33:42 +0300

    Initial commit

---
+++ event-test/module-with-event/build-toolchain_for_desktop/CMakeFiles/mymodule.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mymodule.dir/mymodule.cpp.o"
+  "CMakeFiles/mymodule.dir/main.cpp.o"
+  "sdk/bin/mymodule.pdb"
+  "sdk/bin/mymodule"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mymodule.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ event-test/module-with-event/mymodule.cpp
@@ -0,0 +1,56 @@
+#include "mymodule.h"
+#include <iostream>
+#include <qi/log.hpp>
+#include <alvalue/alvalue.h>
+
+MyModule::MyModule(boost::shared_ptr<AL::ALBroker> broker,
+                   const std::string& name)
+  : AL::ALModule(broker, name), tts_(getParentBroker()), memoryProxy(getParentBroker())
+{
+  // Describe the module here. This will appear on the webpage
+  setModuleDescription("My own custom module.");
+
+  /**
+   * Define callable methods with their descriptions:
+   * This makes the method available to other cpp modules
+   * and to python.
+   * The name given will be the one visible from outside the module.
+   * This method has no parameters or return value to describe
+   * functionName(<method_name>, <class_name>, <method_description>);
+   * BIND_METHOD(<method_reference>);
+   */
+  functionName("generateEvent", getName(), "Generate event");
+  addParam("value", "The data associated with the event");
+  BIND_METHOD(MyModule::generateEvent);
+
+  functionName("callback", getName(), "");
+  BIND_METHOD(MyModule::callback);
+}
+
+MyModule::~MyModule()
+{
+  memoryProxy.unsubscribeToEvent("ExampleEvent", "MyModule");
+}
+
+void MyModule::init()
+{
+  memoryProxy.subscribeToEvent("ExampleEvent", "MyModule", "callback");
+}
+
+void MyModule::generateEvent(const float& value)
+{
+  memoryProxy.raiseEvent("ExampleEvent", value);
+}
+
+void MyModule::callback(){
+  qiLogInfo("Event") << "raise event!" << std::endl;
+  try
+  {
+    tts_.say("sentence to say");
+    qiLogInfo("Event") << "raise event!" << std::endl;
+  }
+  catch(const AL::ALError&)
+  {
+    qiLogError("module.example") << "Could not get proxy to ALTextToSpeech" << std::endl;
+  }
+}
